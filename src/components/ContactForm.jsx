eb24bca96a49f0feb3b58b57fcb7eacff9420781
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2025-04-03 20:55:34 +0200

    Installed ormspree/react and made the contact form work

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,31 +1,45 @@
-export default function ContactForm() {
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+// For more help visit https://formspr.ee/react-help
+
+function ContactForm() {
+  const [state, handleSubmit] = useForm('mwplrnpn');
+  if (state.succeeded) {
+    return <p>Danke für deine Nachricht!</p>;
+  }
   return (
-    <form className='box' id='my-form' action='https://formspree.io/f/mvoeyoly' method='POST'>
+    <form className='box' id='my-form' onSubmit={handleSubmit}>
       <div className='input__wrapper name-input scroll-animation up'>
-        <input type='text' name='name' id='name' className='input__field' placeholder='Your Name' required />
+        <input id='name' type='name' name='name' className='input__field' placeholder='Name' />
         <label htmlFor='name' className='input__label'>
           Name
         </label>
+        <ValidationError prefix='Name' field='name' errors={state.errors} />
       </div>
 
       <div className='input__wrapper email-input scroll-animation up'>
-        <input type='email' name='email' id='email' className='input__field' placeholder='Email' required />
+        <input id='email' type='email' name='email' className='input__field' placeholder='Email' />
         <label htmlFor='email' className='input__label'>
           Email
         </label>
+        <ValidationError prefix='Email' field='email' errors={state.errors} />
       </div>
 
       <div className='input__wrapper message-input scroll-animation up'>
-        <textarea name='message' id='message' className='input__field' placeholder='Nachricht' required></textarea>
+        <textarea id='message' name='message' className='input__field' placeholder='Nachricht' />
         <label htmlFor='message' className='input__label'>
           Nachricht
         </label>
+        <ValidationError prefix='Message' field='message' errors={state.errors} />
       </div>
 
       <div className='submit-input scroll-animation up'>
-        <input id='my-form-button' type='submit' className='input__submit' value='Senden' />
-        <p id='my-form-status'></p>
+        <button type='submit' disabled={state.submitting} id='my-form-button' className='input__submit'>
+          Senden
+        </button>
       </div>
     </form>
   );
 }
+
+export default ContactForm;
