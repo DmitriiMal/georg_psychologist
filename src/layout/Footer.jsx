e9944a9d592def516dd4cc617792349825fa5a96
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2025-04-05 11:17:36 +0200

    moved contact data above contact form

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -11,34 +11,34 @@ function Footer() {
         <div className='container'>
           <h2 className='scroll-animation fade-in-left'>Kontakt</h2>
           <div className='bottom-line scroll-animation fade-in-left'></div>
-          <p className='scroll-animation fade-in-up'>Ich freue mich darauf Sie in einem Erstgespräch kennenzulernen, zu schauen ob wir gemeinsam arbeiten können und der Grundpfeiler Sympathie gegeben ist. Schreiben Sie mir gerne eine E-Mail oder rufen Sie mich persönlich an und hinterlassen mir eine Sprachnachricht- ich rufe Sie folgend ehestmöglich zurück.</p>
-          <ContactForm />
           <ul className='contact-data'>
-            <li>Wildhaber Georg Josef, MSc.</li>
-            <li>
+            <li className='scroll-animation fade-in-up'>Wildhaber Georg Josef, MSc.</li>
+            <li className='scroll-animation fade-in-up'>
               <a href='https://dao-zentrum.at/' target='blank'>
                 <FontAwesomeIcon id='arrow-up-right-from-square' icon={faArrowUpRightFromSquare} />
                 Dao Zentrum
               </a>
             </li>
-            <li>
+            <li className='scroll-animation fade-in-up'>
               <FontAwesomeIcon id='location-dot' icon={faLocationDot} />
               Lustkandlgasse 53/1, 1090 Wien
             </li>
-            <li>
+            <li className='scroll-animation fade-in-up'>
               <a href='mailto:wildhaber@gmx.at'>
                 <FontAwesomeIcon id='envelope' icon={faEnvelope} />
                 wildhaber@gmx.at
               </a>
               {/* Todo: check for custom email in GoDaddy */}
             </li>
-            <li>
+            <li className='scroll-animation fade-in-up'>
               <a href='tel:0660/ 902 67 67'>
                 <FontAwesomeIcon id='phone' icon={faPhone} />
                 0660/ 902 67 67
               </a>
             </li>
           </ul>
+          <p className='scroll-animation fade-in-up'>Ich freue mich darauf Sie in einem Erstgespräch kennenzulernen, zu schauen ob wir gemeinsam arbeiten können und der Grundpfeiler Sympathie gegeben ist. Schreiben Sie mir gerne eine E-Mail oder rufen Sie mich persönlich an und hinterlassen mir eine Sprachnachricht- ich rufe Sie folgend ehestmöglich zurück.</p>
+          <ContactForm />
         </div>
         <div id='copyright'>
           <p>&copy; {new Date().getFullYear()} Wildhaber</p>
