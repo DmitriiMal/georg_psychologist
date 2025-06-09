9cf60f680bc3121346b8e693ec4fb7d3494a5f5c
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2025-06-09 21:26:25 +0200

    added _redirects

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare, faEnvelope, faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-import { scrollToTop } from '@utilities/scrollToTop';
 
 function Footer() {
   return (
